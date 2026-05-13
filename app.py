48c88e5814b0b6e122fdab78798ef64fda6b90c3
Miro <mirotuominen2@gmail.com>
2026-05-13 10:00:39 +0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template, request, redirect
 
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/")
+
+def index():
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+
+    task = request.form.get("task")
+
+    if task:
+        task.append(task)
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
